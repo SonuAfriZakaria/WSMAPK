016b6e17757fbb8cde8bbd0c0b48ab108bef4cdc
SonuAfriZakaria <sonuafirza7@gmail.com>
2024-09-22 21:48:36 +0700

    M4

---
+++ app/src/main/java/com/example/wspc/MainActivityLogin.kt
@@ -0,0 +1,92 @@
+package com.example.wspc
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AlertDialog
+
+
+class MainActivityLogin : AppCompatActivity() {
+
+    private lateinit var editTextTextEmailAddress2: EditText
+    private lateinit var editTextPassword3: EditText
+    private lateinit var buttonShowHide: ImageButton
+    private lateinit var buttonLogin: Button
+    private var isPasswordVisible = false
+    private var registeredPassword: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_login)
+
+        editTextTextEmailAddress2 = findViewById(R.id.editTextTextEmailAddress2)
+        editTextPassword3 = findViewById(R.id.editTextTextPassword3)
+        buttonShowHide = findViewById(R.id.buttonShowHide)
+        buttonLogin = findViewById(R.id.buttonLogin)
+
+        val email = intent.getStringExtra("EMAIL_EXTRA")
+        registeredPassword = intent.getStringExtra("PASSWORD_EXTRA") // Get the registered password
+        email?.let {
+            editTextTextEmailAddress2.setText(it)
+        }
+
+        buttonShowHide.setOnClickListener {
+            togglePasswordVisibility()
+        }
+
+        buttonLogin.setOnClickListener {
+            validateLogin()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun togglePasswordVisibility() {
+        isPasswordVisible = !isPasswordVisible
+        editTextPassword3.transformationMethod = if (isPasswordVisible) {
+            null // Show password
+        } else {
+            android.text.method.PasswordTransformationMethod.getInstance()
+        }
+
+        buttonShowHide.setImageResource(if (isPasswordVisible) {
+            R.drawable.baseline_remove_red_eye_24
+        } else {
+            R.drawable.baseline_panorama_fish_eye_24
+        })
+
+        editTextPassword3.setSelection(editTextPassword3.text.length)
+    }
+
+    private fun validateLogin() {
+        val enteredPassword = editTextPassword3.text.toString()
+
+        if (enteredPassword == registeredPassword) {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            // Proceed to the next activity or functionality
+        } else {
+            showAlertDialog("Peringatan", "Password salah") // Show alert dialog
+        }
+    }
+
+    private fun showAlertDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+}
--- app/src/main/java/com/example/wspc/MainActivityRegis.kt
+++ app/src/main/java/com/example/wspc/MainActivityRegis.kt
@@ -1,21 +1,24 @@
 package com.example.wspc
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 class MainActivityRegis : AppCompatActivity() {
-    private var etTanggal: EditText? = null
-    private var btnTanggal: ImageButton? = null
-    private var etName: EditText? = null
-    private var etEmail: EditText? = null
-    private var btnSubmit: Button? = null
-    private var spinnerGender: Spinner? = null
-
-
+    private lateinit var etTanggal: EditText
+    private lateinit var btnTanggal: ImageButton
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnSubmit: Button
+    private lateinit var spinnerGender: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,57 +29,70 @@ class MainActivityRegis : AppCompatActivity() {
         btnTanggal = findViewById(R.id.btnTanggal)
         etName = findViewById(R.id.editTextText3)
         etEmail = findViewById(R.id.editTextTextEmailAddress)
+        etPassword = findViewById(R.id.editTextTextPassword)
+        etConfirmPassword = findViewById(R.id.editTextTextPassword2)
         btnSubmit = findViewById(R.id.buttonSubmit)
         spinnerGender = findViewById(R.id.spinnerGender)
 
-
+        // Setup Spinner untuk Gender
         val genderOptions = resources.getStringArray(R.array.gender_options)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerGender?.adapter = adapter
+        spinnerGender.adapter = adapter
 
-        btnTanggal?.setOnClickListener {
-            val calendar = Calendar.getInstance()
-            val tahun = calendar[Calendar.YEAR]
-            val bulan = calendar[Calendar.MONTH]
-            val tanggal = calendar[Calendar.DAY_OF_MONTH]
-            val dialog = DatePickerDialog(
-                this,
-                { _, year, month, dayOfMonth ->
-                    etTanggal?.setText("$dayOfMonth - ${month + 1} - $year")
-                }, tahun, bulan, tanggal
-            )
-            dialog.show()
+        btnTanggal.setOnClickListener {
+            showDatePicker()
         }
 
-        etTanggal?.setOnClickListener {
-            val calendar = Calendar.getInstance()
-            val tahun = calendar[Calendar.YEAR]
-            val bulan = calendar[Calendar.MONTH]
-            val tanggal = calendar[Calendar.DAY_OF_MONTH]
-            val dialog = DatePickerDialog(
-                this,
-                { _, year, month, dayOfMonth ->
-                    etTanggal?.setText("$dayOfMonth - ${month + 1} - $year")
-                }, tahun, bulan, tanggal
-            )
-            dialog.show()
+        etTanggal.setOnClickListener {
+            showDatePicker()
         }
 
-        btnSubmit?.setOnClickListener {
-            val name = etName?.text.toString()
-            val email = etEmail?.text.toString()
-
-            if (name.isNotEmpty() && email.isNotEmpty()) {
-                Toast.makeText(this, "Registrasi berhasil dengan Nama: $name\nEmail: $email", Toast.LENGTH_LONG).show()
-
-            } else {
-                Toast.makeText(this, "Nama dan Email harus terisi", Toast.LENGTH_SHORT).show()
-            }
+        btnSubmit.setOnClickListener {
+            validateAndSubmit()
         }
     }
 
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val tahun = calendar[Calendar.YEAR]
+        val bulan = calendar[Calendar.MONTH]
+        val tanggal = calendar[Calendar.DAY_OF_MONTH]
+        val dialog = DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                // Create a Calendar instance with the selected date
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, month, dayOfMonth)
 
+                // Format the date
+                val dateFormat = SimpleDateFormat("d MMMM yyyy", Locale("id")) // Use Indonesian locale
+                val formattedDate = dateFormat.format(selectedDate.time)
+
+                etTanggal.setText(formattedDate) // Set the formatted date to the EditText
+            }, tahun, bulan, tanggal
+        )
+        dialog.show()
+    }
+
+    private fun validateAndSubmit() {
+        val name = etName.text.toString()
+        val email = etEmail.text.toString()
+        val tanggal = etTanggal.text.toString()
+        val password = etPassword.text.toString()
+        val confirmPassword = etConfirmPassword.text.toString()
+
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Password dan Konfirmasi password harus sama", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Toast.makeText(this, "Registrasi berhasil", Toast.LENGTH_LONG).show()
+
+        val intentDestination = Intent(this, MainActivityLogin::class.java).apply {
+            putExtra("EMAIL_EXTRA", email)
+            putExtra("PASSWORD_EXTRA", password)
+        }
+        startActivity(intentDestination)
+    }
 }
-
-
