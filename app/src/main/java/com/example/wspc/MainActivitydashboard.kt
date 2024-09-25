e83cc0cabf6c2c4270fab78f2d8d0c83b5eda878
SonuAfriZakaria <sonuafirza7@gmail.com>
2024-09-25 12:06:11 +0700

    26sept2024

--- app/src/main/java/com/example/wspc/MainActivitydashboard.kt
+++ app/src/main/java/com/example/wspc/MainActivitydashboard.kt
@@ -22,13 +22,13 @@ class MainActivitydashboard : AppCompatActivity() {
         val alamat = intent.getStringExtra("ALAMAT_EXTRA")
         val password = intent.getStringExtra("PASSWORD_EXTRA")
 
-        findViewById<TextView>(R.id.textView2).text = "USERNAME: $username"
-        findViewById<TextView>(R.id.textView3).text = "NAMA: $fullname"
-        findViewById<TextView>(R.id.lahir).text = "TANGGAL LAHIR: $tanggalLahir"
-        findViewById<TextView>(R.id.email).text = "EMAIL: $email"
-        findViewById<TextView>(R.id.gender).text = "GENDER: $gender"
-        findViewById<TextView>(R.id.telp).text = "NOMOR TELEPON: $nomorTelepon"
-        findViewById<TextView>(R.id.alamat).text = "ALAMAT: $alamat"
+        findViewById<TextView>(R.id.textView2).text = "USERNAME : $username"
+        findViewById<TextView>(R.id.textView3).text = "NAMA : $fullname"
+        findViewById<TextView>(R.id.lahir).text = "TANGGAL LAHIR : $tanggalLahir"
+        findViewById<TextView>(R.id.email).text = "EMAIL : $email"
+        findViewById<TextView>(R.id.gender).text = "GENDER : $gender"
+        findViewById<TextView>(R.id.telp).text = "TELEPON : $nomorTelepon"
+        findViewById<TextView>(R.id.alamat).text = "ALAMAT : $alamat"
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.username)) { v, insets ->
